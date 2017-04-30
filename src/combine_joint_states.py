269e802c15780a28f6eaf29b4ef7d40570f22ff9
Logan <Logan.C.Farrell@gmail.com>
2017-04-30 17:43:09 -0500

    updated to intelligently take what exists and to combine grippers in too

--- src/combine_joint_states.py
+++ src/combine_joint_states.py
@@ -8,12 +8,18 @@ from sensor_msgs.msg import JointState
 class CombineJointStates(object):
 
 
-    def callback_left(self, data):
+    def callback_left_arm(self, data):
         rospy.logdebug('left joint states = {}'.format(data))
-        self.left_data = data
-    def callback_right(self, data):
+        self.left_arm_data = data
+    def callback_right_arm(self, data):
         rospy.logdebug('right joint states = {}'.format(data))
-        self.right_data = data
+        self.right_arm_data = data
+    def callback_left_gripper(self, data):
+        rospy.logdebug('left joint states = {}'.format(data))
+        self.left_gripper_data = data
+    def callback_right_gripper(self, data):
+        rospy.logdebug('right joint states = {}'.format(data))
+        self.right_gripper_data = data
     def listener(self):
 
         # In ROS, nodes are uniquely named. If two nodes with the same
@@ -22,28 +28,57 @@ class CombineJointStates(object):
         # name for our 'listener' node so that multiple listeners can
         # run simultaneously.
 
-        rospy.Subscriber("left_manipulator/joint_states", JointState, self.callback_left)
-        rospy.Subscriber("right_manipulator/joint_states", JointState , self.callback_right)
+        rospy.Subscriber("left_manipulator/joint_states", JointState, self.callback_left_arm)
+        rospy.Subscriber("right_manipulator/joint_states", JointState , self.callback_right_arm)
+        rospy.Subscriber("left_gripper/joint_states", JointState, self.callback_left_gripper)
+        rospy.Subscriber("right_gripper/joint_states", JointState , self.callback_right_gripper)
 
         # spin() simply keeps python from exiting until this node is stopped
         joint_state_pub = rospy.Publisher('/joint_states', JointState, queue_size=10)
         rate = rospy.Rate(100)
 
         while not rospy.is_shutdown():
-            if self.left_data is not None and self.right_data is not None:
-                output = JointState()
-                output.header = self.left_data.header
-                output.name = self.left_data.name + self.right_data.name
-                output.position = self.left_data.position + self.right_data.position
-                output.velocity = self.left_data.velocity + self.right_data.velocity
-                output.effort = self.left_data.effort + self.right_data.effort
-                rospy.loginfo('output = {}'.format(output))
-                joint_state_pub.publish(output)
+            output = JointState()
+            output.name=list()
+            output.position=list()
+            output.velocity=list()
+            output.effort=list()
+            if self.left_arm_data is not None:
+                output.header = self.left_arm_data.header
+                output.name = output.name + self.left_arm_data.name
+                output.position = output.position + list(self.left_arm_data.position)
+                output.velocity = output.velocity + list(self.left_arm_data.velocity)
+                output.effort = output.velocity + list(self.left_arm_data.effort)
+            if self.right_arm_data is not None:
+                output.header = self.right_arm_data.header
+                output.name = output.name + self.right_arm_data.name
+                rospy.logdebug('output.name = {}'.format(output.name))
+                output.position = output.position + list(self.right_arm_data.position)
+                output.velocity = output.velocity + list(self.right_arm_data.velocity)
+                output.effort = output.velocity + list(self.right_arm_data.effort)
+            if self.left_gripper_data is not None:
+                output.header = self.left_gripper_data.header
+                output.name = output.name + self.left_gripper_data.name
+                output.position = output.position + list(self.left_gripper_data.position)
+                output.velocity = output.velocity + list(self.left_gripper_data.velocity)
+                output.effort = output.velocity + list(self.left_gripper_data.effort)
+            if self.right_gripper_data is not None:
+                output.header = self.right_gripper_data.header
+                output.name = output.name + self.right_gripper_data.name
+                output.position = output.position + list(self.right_gripper_data.position)
+                output.velocity = output.velocity + list(self.right_gripper_data.velocity)
+                output.effort = output.velocity + list(self.right_gripper_data.effort)
+
+            joint_state_pub.publish(output)
+            rospy.logdebug('output = {}'.format(output))
+
             rate.sleep()
 
     def __init__(self):
-        self.left_data = None
-        self.right_data = None
+        self.left_arm_data = None
+        self.right_arm_data = None
+        self.left_gripper_data = None
+        self.right_gripper_data = None
 
 if __name__ == '__main__':
       rospy.init_node('joint_states_combine')
