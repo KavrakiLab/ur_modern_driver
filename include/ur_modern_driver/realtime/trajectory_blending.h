cac50da093aba0c16e1bd0ccb80d6cd46aa41317
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2017-06-07 17:14:20 -0500

    Blender is in, working on the trajectory following now.

--- include/ur_modern_driver/realtime/trajectory_blending.h
+++ include/ur_modern_driver/realtime/trajectory_blending.h
@@ -2,6 +2,7 @@
  * trajectory_blending.h
  * Header file for a process that converts sns_msg_path_dense to
  * sns_msg_motor_state through parabolic blending.
+ * Inspired by code by Zak Kingston.
  *
  * Copyright 2017 Bryce Willey
  *
@@ -31,12 +32,76 @@
 #include <ach/experimental.h>
 
 /**
- * The event handler for trajectory blending: accepts waypoint paths
- * (sns_msg_path_dense) and sends the path off to the driver as a series of
- * sns_msg_motor_refs.
+ * The context needed for continually following a trajectory.
  */
-enum ach_status handle_waypoint_path(void *cx_, void *msg_, size_t msg_size);
+struct traj_follow_cx_init {
+    /** The channel to send motor references to (ideally the actual robot driver). */
+    struct ach_channel ref_channel;
 
+    /** How to send motor references to the robot driver. */
+    enum sns_motor_mode mode;
 
+    /** The proportional gain when following a trajectory. */
+    double k_p;
+};
+
+struct traj_follow_cx {
+    /**
+     * The initial parameters that this follow context can be seeded from the
+     * blend context.
+     */
+    const struct traj_follow_cx_init *init;
+
+    /**
+     * The blended path to follow.
+     */
+    struct aa_ct_seg_list *seg_list;
+
+    /**
+     * The time that the execution of this path was started.
+     */
+    double start_time;
+};
+
+/**
+ * The context needed for handling waypoint paths.
+ */
+struct traj_blend_cx {
+    /** The limits of the robot for which the path is being blended. */
+    struct aa_ct_state *limits;
+
+    /**
+     * Used to begin the process of trajectory following after the waypoints
+     * are blended.
+     */
+    struct traj_follow_cx_init *follow_cx;
+};
+
+/**
+ * Turns sns dense paths into amino waypoint lists.
+ * reg is the amino memory region from which the waypoint list will be allocated.
+ *
+ * NOTE: the t0 and period fields of the path argument are not used: the given
+ * path is simply blended, and motor refs are sent with the current time and
+ * motor frequency instead.
+ */
+struct aa_ct_pt_list *sns_to_amino_path(struct aa_mem_region *reg, struct sns_msg_path_dense *path);
+
+/**
+ * \brief The event handler for trajectory blending.
+ *
+ * cx_ is a traj_blend_cx, and msg_ is a waypoint path (sns_msg_path_dense).
+ * Blends the waypoints and sends the first sns_msg_motor_ref to the driver.
+ */
+enum ach_status handle_blend_waypoint(void *cx_, void *msg_, size_t msg_size);
+
+/**
+ * \brief The event handler for following a blended trajectory.
+ *
+ * cx_ is a traj_follow_cx, and msg_ is a sns_motor_state.
+ * Exerts KD-control on the robot to ensure that it follows the blended
+ * trajectory.
+ */
+enum ach_status handle_follow_state(void *cx_, void *msg_, size_t msg_size);
 
 #endif // UR_MODERN_DRIVER_REALTIME_TRAJECTORY_BLENDING_H
--- src/realtime/trajectory_blending.cpp
+++ src/realtime/trajectory_blending.cpp
@@ -22,6 +22,7 @@
 #define PACKAGE_VERSION "1.0"
 #endif
 
+#include <sns/msg.h>
 #include "ur_modern_driver/realtime/trajectory_blending.h"
 
 int main(int argc, char **argv)
@@ -36,5 +37,45 @@ int main(int argc, char **argv)
     return 0;
 }
 
-enum ach_status handle_waypoint_path(void *cx_, void *msg_, size_t msg_size) {}
+enum ach_status handle_blend_waypoint(void *cx_, void *msg_, size_t msg_size)
+{
+    struct traj_blend_cx *cx = (struct traj_blend_cx *)cx;
+    struct sns_msg_path_dense *msg = (struct sns_msg_path_dense *)msg_;
 
+    if (sns_msg_motor_state_check_size(msg, msg_size)) {
+        SNS_LOG(LOG_ERR, "Mismatched message size on channel\n");
+    } else {
+        /* Turn this message into descritized position/velocity commands. */
+        struct aa_mem_region reg;
+        aa_mem_region_init(&reg, 512);
+        struct aa_ct_pt_list *aa_list = sns_to_amino_path(reg, msg);
+
+        // Blend the path.
+        struct aa_ct_seg_list *seg_list = aa_ct_tjq_pb_generate(&reg, pt_list, cx->limits);
+
+        // Initialize the follow context.
+        struct traj_blend_cx follow_context;
+
+    }
+}
+
+enum ach_status handle_follow_state(void *cx_, void *msg_, size_t msg_size)
+{
+
+}
+
+struct aa_ct_pt_list *sns_to_amino_path(struct aa_mem_region *reg, struct sns_msg_path_dense *path)
+{
+    struct aa_ct_pt_list *aa_list = aa_ct_pt_list_create(reg);
+
+    int n_steps = path->n_steps;
+    int n_q = path->n_dof;
+    for (int i = 0; i < n_steps; i++)
+    {
+        struct aa_ct_state *state = aa_ct_state_alloc(reg, n_q, 0);
+        AA_MEM_CPY(state->q, path->x + i * n_steps, n_q);
+        aa_ct_pt_list_add(aa_list, state);
+    }
+
+    return aa_list;
+}
